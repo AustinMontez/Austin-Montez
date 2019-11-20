bb7706126828d042a8d3c80a563a1b4f0d7da513
Austin Montez <57913752+AustinMontez@users.noreply.github.com>
2019-11-20 11:32:20 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,18 +1,48 @@
-//Unit 1 Assignment: Austin_Montez, Dr.T
-//10-15-2019
-#include "Physics_Calculator_Austin_Montez.h"
+#include "Input_Validation_Extended.h"
+#include "Ship_Game.h"
+#include "Tic_tac_toe.h"
+using namespace std;
 
-int main() 
+void showMenu();
+
+int main()
 {
-  int option = 0;
+  int userOption = 0;
 
   do
   {
     showMenu();
-    cout << "\nPlease enter an option: ";
-    option = validateInt(option);
-    handleOption(option);
-  }while(option != 7);
-  cout << "\nGoodbye. " << endl;
-  return 0;
+
+    userOption = validateInt(userOption);
+    if(userOption == 1)
+    {
+      playShip();
+    }
+
+    else if(userOption == 2)
+    {
+      ticTac();
+    }
+    else if(userOption ==4)
+    {
+      cout << "\033[2J\033[1;1H";
+    }
+
+    else
+    {
+      cout << "Invalid input, try again d00000d." << endl;
+    }
+  }while(userOption != 3);
+  {
+    cout << "C ya!" << endl;
+  }
+}
+void showMenu()
+{
+  cout << "\nMENU" << endl;
+  cout << "------------" << endl;
+  cout << "1: Ship Game" << endl;
+  cout << "2: Tic Tac Toe" << endl;
+  cout << "3: Exit"  << endl;
+  cout << "4: Clear Screen" << endl;
 }
